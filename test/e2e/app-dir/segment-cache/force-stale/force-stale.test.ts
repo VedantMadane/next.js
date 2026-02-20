59071dcc09d968b36b9cc8451cae9d538279627a
Hendrik Liebau <mail@hendrik-liebau.de>
2026-02-20 09:42:04 -0800

    Revert "Disable Cache Components for `force-stale` test suite"
    This reverts commit 51549479c568f17f813fd06bba3172aa54149d6c.

--- test/e2e/app-dir/segment-cache/force-stale/force-stale.test.ts
+++ test/e2e/app-dir/segment-cache/force-stale/force-stale.test.ts
@@ -85,12 +85,15 @@ describe('force stale', () => {
       // already navigated to this page, the data should be in the bfcache.
       // The prefetch should reuse the bfcache data instead of making a new
       // request to the server.
-      await act(async () => {
-        const toggleLinkVisibility = await browser.elementByCss(
-          'input[data-link-accordion="/dynamic"]'
-        )
-        await toggleLinkVisibility.click()
-      }, 'no-requests')
+      await act(
+        async () => {
+          const toggleLinkVisibility = await browser.elementByCss(
+            'input[data-link-accordion="/dynamic"]'
+          )
+          await toggleLinkVisibility.click()
+        },
+        { includes: 'Dynamic page content', block: 'reject' }
+      )
     }
   )
 })
